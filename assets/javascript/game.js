445ed40191382db7efb60e83387f25c3e19f6ab9
Giovanna Roman <gromanbb@gmail.com>
2019-09-06 21:40:47 -0500

    Modified game.js to add function for validating guesses entered. Added place holders for displayin results in index.html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,13 +4,16 @@ var wins = 0;
 var displayMovie = [];
 var numGuessesLeft = 12;
 var guessedLetters = [];
+// ojo
+var displayWins = [];
+var displayGuessesLeft = [];
+var displayGuessedLetters = [];
+
 
 var disneyMovies = ["ZOOTOPIA", "MOANA", "TANGLED", "ALLADIN"];
 /* "BRAVE", "INCREDIBLES", "MULAN", "RATATOUILLE", "HERCULES", "CINDERELLA", "POCAHONTAS", "ARISTOCATS", "PINOCCHIO", "FANTASIA", "MALEFICENT", "BAMBI", "PLANES", "FROZEN", "TARZAN" */
 
-var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-var compMovie = [];
+var compMovie = "";
 var compIndex = 0;
 var userLetter = "";
 var guessedMovie = "";
@@ -32,16 +35,96 @@ function pickRandomMovie() {
     // ojo
     console.log("pickRandomMovie() executed");
     alert("Pick Random Movie...");
+
+    var compIndex = Math.floor(Math.random() * disneyMovies.length);
+    var compMovie = disneyMovies[compIndex];
+    console.log("compIndex: " + compIndex);
+    console.log("Picked Movie: " + compMovie);
+    console.log("compMovie length " + compMovie.length);
+
+    // ojo Display/Render
+    for (var i = 0; i < compMovie.length; i++) {
+        displayMovie[i] = "_";
+        console.log("displayMovie[" + i + "]: " + displayMovie[i]);
+    }
+    
+    console.log("numGuessesLeft" + numGuessesLeft);
+
+    // Loop until there are no more guesses
+    for (var z = numGuessesLeft; z > 0; z--) {
+        // Validate keyed letter
+        document.onkeyup = function(event) {
+            userLetter = event.key.toUpperCase();
+            console.log("userLetter: " + userLetter);
+/*
+            // Validate that user keyed only letters
+            if (!userLetter.value.match(/^[A-Z]+$/)) {
+                alert('Only letters are allowed. Try again!');
+                // ojo
+                console.log("Try again!");
+            } else {
+                */
+                for (var i = 0; i < compMovie.length; i++) {
+                    if (userLetter === compMovie[i]) {
+                        guessedMovie = guessedMovie + userLetter;
+                        console.log("guessedMovie: " + guessedMovie);
+                        displayMovie[i] = userLetter;
+                        console.log("displayMovie[i]: " + displayMovie[i] + "/ i:" + i);
+
+                        //ojo check if already in guessedLetters???
+                    } else {
+                        // numGuessesLeft--;
+                        console.log("numGuessesLeft: " + numGuessesLeft);
+
+                        guessedLetters.push(userLetter);
+                        displayGuessedLetters.push(userLetter);
+
+                        // ojo
+                        for (w = 0; w > guessedLetters.length; w++) {
+                            console_log("guessedLetters[w]: " + guessedLetters[w] + "/ w:" + w);
+
+                        }
+    
+                        // ojo Display changes here????
+                        // displayStats();                        
+                    }            
+                }
+            // }
+        }
+    }
+
 }
 
 function validateGuessedMovie() {
     // ojo
     console.log("validateGuessedMovie() executed");
     alert("Validate Guessed Movie...");
+ 
+/*
+    if (guessedMovie === compMovie ) {
+        wins++;
+        numGuessesLeft = 12;
+        userLetter = "";
+        guessedMovie = "";
+        compIndex = 0;
+        compMovie = "";
+        displayMovie = "";  // ojo how to initialize an array???
+        displayMovie = [];
+        guessedLetters = [];
+        displayGuessesLeft = [];
+        displayGuessedLetters = [];
 
+    }
+*/
 }
 
-function hangman () {
+function celebrateWin() {
+    // ojo
+    console.log("celebrateWin() executed");
+    alert("Celebrate Win!!!");
+}
+
+function hangman() {
 
     displayStats();
     //ojo
@@ -58,16 +141,31 @@ function hangman () {
     console.log("Invoked validateGuessedMovie() from hangman()...");
     alert("Invoked validateGuessedMovie() from hangman()...");
 
-    // Execute hangman() in a recursive manner until game is lost!!!
-    // ojo force winning first game to validate logic!
-    if (isFirstGame) {
-        wins = 1;
-        isFirstGame = false;
-        console.log("wins = " + wins + " / isFirstGame = " + isFirstGame);
+    // Execute hangman() recursively until game is over
+    if (wins !== 0) {
+        // ojo
+        console.log("Invoked hangman() after winning...");
+        alert("Invoked hangman() after winning...");
+
+        //ojo
+        celebrateWin();
+        console.log("Invoked celebrateWin() after winning...");
+        alert("Invoked celebrateWin() after winning...");
+
         hangman();
     }
-    else {
-        wins = 0;
+    else if (wins === 0) {
+        // ojo
+        console.log("Invoke displayStats() after loosing...");
+        alert("Invoke displayStats() after loosing...");
+
+        displayStats();
+
+        // ojo
+        console.log("Invoke displayGameOver() after loosing...");
+        alert("Invoke displayGameOver() after loosing...");
+
+        displayGameOver();
     }
 }
 
@@ -79,34 +177,8 @@ function displayGameOver() {
 // MAIN PROCESS
 // ==============================================================================
 
-// Play game at least one Time
-// ojo
 console.log("Invoke hangman() for the first time...");
 alert("Invoke hangman() for the first time...");
 
 hangman();
 
-// ojo check wins
-console.log("wins = " + wins);
-
-// ojo move below logic to hangman function
-if (wins !== 0) {
-    // ojo
-    console.log("Invoked hangman() after winning...");
-    alert("Invoked hangman() after winning...");
-
-    hangman();
-}
-else if ( wins === 0) {
-    // ojo
-    console.log("Invoke displayStats() after loosing...");
-    alert("Invoke displayStats() after loosing...");
-
-    displayStats();
-
-    // ojo
-    console.log("Invoke displayGameOver() after loosing...");
-    alert("Invoke displayGameOver() after loosing...");
-
-    displayGameOver();
-}
