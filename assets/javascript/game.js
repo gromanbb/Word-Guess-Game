b938432df6c7db7e17ffb38285f8bc82309844e2
Giovanna Roman <gromanbb@gmail.com>
2019-09-09 22:46:28 -0500

    Fixed code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,51 +1,52 @@
 // VARIABLES
 // ==============================================================================
 
-var disneyMovies = ["ZOOTOPIA", "MOANA", "TANGLED", "ALLADIN", "BRAVE", "INCREDIBLES", "MULAN", "RATATOUILLE", "HERCULES", "CINDERELLA", "POCAHONTAS", "ARISTOCATS", "PINOCCHIO", "FANTASIA", "MALEFICENT", "BAMBI", "PLANES", "FROZEN", "TARZAN"]
+var disneyMovies = ["ZOOTOPIA", "INCREDIBLES", "HERCULES", "POCAHONTAS", "MOANA", "RATATOUILLE", "MALEFICENT", "MULAN", "TANGLED", "CINDERELLA", "PINOCCHIO", "ALLADIN", "PLANES", "FANTASIA", "ARISTOCATS", "FROZEN",  "TARZAN", "BAMBI", "BRAVE"]
 
 var score = 0;
 var wins = 0;
-var displayWins = [];
-var strDivImage = "";
 
-var compMovie = "";
 var compIndex = 0;
+var compMovie = "";
+var guessedMovie = [];
 var displayMovie = [];
 
 var maxGuessedLetters = 12;
 var numGuessesLeft = 12;
-var displayGuessesLeft = [];
 
 var userLetter = "";
 var isCapsLetter = false;
 
 var guessedLetters = [];
-var displayGuessedLetters = [];
-
-var guessedMovie = [];
+var strGuessedLetters = "";
 
 var arrLength = 0;
 var diffLetters = 0;
 var strGuessedMovie = "";
-var strGuessedLetters = "";
+var strDisplayMovie = "";
 var strEqualLetters = "";
 
-var liveGame = false;
+var liveGame = true;
 
 // FUNCTIONS
 // ==============================================================================
 function displayStats() {
-    //console.log("displayStats()--> Function executed");
+    console.log("displayStats()--> Function executed");
 
-    document.querySelector("#pWins").innerHTML = "<h5>Wins: " + score + "</h5>";
-    document.querySelector("#pCurrentWord").innerHTML = "<h5>Current Word:  " + strGuessedMovie + "</h5>";
-    document.querySelector("#pGuessesLeft").innerHTML = "<h5>Number of guesses remaining: " + numGuessesLeft + "</h5>";
-    document.querySelector("#pLettersGuessed").innerHTML = "<h5>Letters already guessed: " + strGuessedLetters + "</h5>";
-
+    document.querySelector("#pWins").innerHTML = "<p>" + score + "</p>";
+    document.querySelector("#pCurrentWord").innerHTML = "<p>" + strDisplayMovie + "</p>";
+    document.querySelector("#pGuessesLeft").innerHTML = "<p>" + numGuessesLeft + "</p>";
+    document.querySelector("#pLettersGuessed").innerHTML = "<p>" + strGuessedLetters + "</p>";
 }
 
 function pickRandomMovie() {
-    //console.log("pickRandomMovie()--> Function executed.");
+    console.log("pickRandomMovie()--> Function executed.");
+
+    compIndex = 0;
+    compMovie = "";
+    guessedMovie = [];
+    displayMovie = [];
+    strDisplayMovie = "";
 
     compIndex = Math.floor(Math.random() * disneyMovies.length);
     compMovie = disneyMovies[compIndex];
@@ -54,124 +55,18 @@ function pickRandomMovie() {
     console.log("pickRandomMovie()--> compMovie length " + compMovie.length);
 
     for (var i = 0; i < compMovie.length; i++) {
-        displayMovie[i] = "_";
-        //console.log("pickRandomMovie()-->  displayMovie[" + i + "]: " + displayMovie[i]);
         guessedMovie[i] = "_";
-        //console.log("pickRandomMovie()-->  guessedMovie[" + i + "]: " + guessedMovie[i]);
+        // console.log("pickRandomMovie()-->  guessedMovie[" + i + "]: " + guessedMovie[i]);
+        displayMovie[i] = "_" + " ";
+        // console.log("pickRandomMovie()-->  displayMovie[" + i + "]: " + displayMovie[i]);
     }
-    liveGame = true;
-}
-
-
-
-document.onkeyup = function (event) {
-    if (liveGame) {
-        console.log("onkeyup event!");
-
-        userLetter = event.key.toUpperCase();
-        console.log("userLetter: " + userLetter);
-
-        // Validate that user keyed only letters
-        isCapsLetter = isLetter(userLetter);
-        if (!isCapsLetter) {
-            console.log("Only letters are allowed. Try again!");
-        } else {
-            for (var i = 0; i < compMovie.length; i++) {
-
-                if (userLetter === compMovie[i]) {
-                    guessedMovie[i] = userLetter;
-                    displayMovie[i] = userLetter;
-                }
-            }
-
-            arrLength = 0;
-            diffLetters = 0;
-            strGuessedLetters = "";
-            strEqualLetters = "";
-
-            arrLength = guessedLetters.length;
-            for (var j = 0; j < arrLength; j++) {
-
-                if (userLetter !== guessedLetters[j]) {
-                    diffLetters++;
-                }
-            }
-
-            if (diffLetters === arrLength) {
-                guessedLetters.push(userLetter);
-                strGuessedLetters = guessedLetters.join('');
-                displayGuessedLetters.push(userLetter);
-
-                //console.log("strGuessedLetters: " + strGuessedLetters + "; guessedLetters.length: " + guessedLetters.length);
-            }
-
-            strGuessedLetters = guessedLetters.join('');
-            console.log("strGuessedLetters.length: " + strGuessedLetters.length + "; strGuessedLetters: " + strGuessedLetters);
-
-            strEqualLetters = getUniqueCharacters(guessedMovie);
-            console.log("strEqualLetters.length: " + strEqualLetters.length + "; strEqualLetters: " + strEqualLetters);
-
-            numGuessesLeft = maxGuessedLetters - (strGuessedLetters.length - strEqualLetters.length);
-            console.log("numGuessesLeft: " + numGuessesLeft + "; strGuessedLetters.length: " + strGuessedLetters.length + "; strEqualLetters.length: " + strEqualLetters.length);
-
-            strGuessedMovie = guessedMovie.join('');
-            console.log("strGuessedMovie.length: " + strGuessedMovie.length + "; strGuessedMovie: " + strGuessedMovie);
-
-            displayStats();
-
-            if (strGuessedMovie === compMovie) {
-                //console.log("strGuessedMovie: " + strGuessedMovie + "; compMovie: " + compMovie);
-
-                wins++;
-                //console.log("wins: " + wins);
-
-                score = score + wins;
-                console.log("score: " + score);
-            } else {
-                if (numGuessesLeft === 0) {
-                    wins = 0;
-                }
-            }
-        }   // End else isCapsLetter
-
-    }
-
-
-}   // End onkeyup event
-
-function hangman() {
-
-    displayStats();
-    pickRandomMovie();
-    validateGuessedMovie();
-
-    // Execute hangman() recursively until game over
-    if (wins !== 0) {
-
-        celebrateWin();
-        displayStats();
-
-        wins = 0;
-        numGuessesLeft = 12;
-        strGuessedMovie = "";
-        hangman();
-    }
-    else if (wins === 0) {
-        displayStats();
-        displayGameOver();
-    }
+    strDisplayMovie = displayMovie.join("");
 }
 
 function displayGameOver() {
-    //console.log("displayGameOver()--> Function executed");
-}
+    console.log("displayGameOver()--> Function executed");
 
-function celebrateWin() {
-    console.log("celebrateWin()--> Function executed...");
-
-    strDivImage = '<img src="../assets/images/' + strGuessedMovie + '.jpg">';
-    document.querySelector("#divImage").innerHTML = strDivImage;
-
+    alert("You lost!");
 }
 
 // Retrieve unique characters from string
@@ -200,7 +95,90 @@ function isLetter(letterAZ) {
 
 // MAIN PROCESS
 // ==============================================================================
+console.log("Main--> Start game!");
 
-console.log("Main--> Invoke hangman()");
-hangman();
+pickRandomMovie();
+displayStats();
+
+document.onkeyup = function (event) {
+    if (liveGame) {
+        // console.log("onkeyup event!");
+        
+        userLetter = event.key.toUpperCase();
+        // console.log("userLetter: " + userLetter);
+
+        // Validate that user keyed only letters
+        isCapsLetter = isLetter(userLetter);
+        if (!isCapsLetter) {
+            console.log("Only letters are allowed. Try again!");
+        } else {
+            document.querySelector("#divImage").innerHTML = '<img src="">';
+            for (var i = 0; i < compMovie.length; i++) {
+                if (userLetter === compMovie[i]) {
+                    guessedMovie[i] = userLetter;
+                    displayMovie[i] = userLetter + " ";
+                }
+            }
+            strGuessedMovie = guessedMovie.join("");
+            // console.log("strGuessedMovie: " + strGuessedMovie + "; strGuessedMovie.length: " + strGuessedMovie.length);
+            strDisplayMovie = displayMovie.join("");
+            // console.log("strDisplayMovie: " + strDisplayMovie + "; strDisplayMovie.length: " + strDisplayMovie.length);
+
+            arrLength = 0;
+            diffLetters = 0;
+            strGuessedLetters = "";
+            strEqualLetters = "";
+
+            arrLength = guessedLetters.length;
+            for (var j = 0; j < arrLength; j++) {
+                if (userLetter !== guessedLetters[j]) {
+                    diffLetters++;
+                }
+            }
+
+            if (diffLetters === arrLength) {
+                guessedLetters.push(userLetter);
+                strGuessedLetters = guessedLetters.join("");
+            }
+
+            strGuessedLetters = guessedLetters.join("");
+            // console.log("strGuessedLetters: " + strGuessedLetters + "; strGuessedLetters.length: " + strGuessedLetters.length);
+            strEqualLetters = getUniqueCharacters(guessedMovie);
+            // console.log("strEqualLetters: " + strEqualLetters + "; strEqualLetters.length: " + strEqualLetters.length);
+            numGuessesLeft = maxGuessedLetters - (strGuessedLetters.length - strEqualLetters.length);
+            // console.log("numGuessesLeft: " + numGuessesLeft + "; strGuessedLetters.length: " + strGuessedLetters.length + "; strEqualLetters.length: " + strEqualLetters.length);
+
+            displayStats();
+
+            if (strGuessedMovie === compMovie) {
+                wins++;
+                score = score + wins;
+
+                displayStats();
+                document.querySelector("#divImage").innerHTML = '<img src="assets/images/' + strGuessedMovie + '.jpg">';
+
+                wins = 0;
+                numGuessesLeft = 12;
+                guessedLetters = [];
+                displayMovie = [];
+                strGuessedLetters = "";
+                strEqualLetters = "";
+                strGuessedMovie = "";
+                strDisplayMovie = "";
+
+                pickRandomMovie();
+                displayStats();
+            
+            } else {
+                if (numGuessesLeft === 0) {
+                    wins = 0;
+
+                    displayStats();
+                    displayGameOver();
+                    liveGame = false;
+                }
+            }   // End else strGuessedMovie
+        }   // End else isCapsLetter
+    }   // End liveGame
+}   // End onkeyup event
 
